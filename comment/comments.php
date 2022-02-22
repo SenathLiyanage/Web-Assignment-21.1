eff90673307e7d17fff3a6dc530e01de3162af76
senuduhasal <senuduhasal@gmail.com>
2022-02-22 22:51:16 +0530

    get database connection

--- comment/comments.php
+++ comment/comments.php
@@ -1,6 +1,17 @@
 <?php
-  echo "senudu";
-  echo "dehiaththakandiya";
+  
+  $sName = "localhost";
+$uName = "senudu";
+$pass = "2022";
+$db_name = "sdg";
+
+try {
+    $conn = new PDO("mysql:host=$sName;dbname=$db_name", 
+                    $uName, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch(PDOException $e){
+  echo "Connection failed : ". $e->getMessage();
+}
  ?>
 
 <!DOCTYPE html>
