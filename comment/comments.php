dcb2e97a5f02a12b03f7f2f4431d9e29240f5a9a
senuduhasal <senuduhasal@gmail.com>
2022-02-22 22:54:04 +0530

    add action and method
    create a action to comments.php . create a post method

--- comment/comments.php
+++ comment/comments.php
@@ -203,18 +203,18 @@ try {
                     <b>Comments</b>
             </p>
             <br>
-  <form class="form-block">
+<form class="form-block" action="comments.php" method="post">
     <div class="row">
       <div class="col-xs-12 col-sm-6">
         <div class="form-group fl_icon">
           <div class="icon"><i class="fa fa-user"></i></div>
-          <input class="form-input" type="text" placeholder="Your name" name="user">
+          <input class="form-input" type="text" placeholder="Your name" name="user" style="color: black;">
         </div>
       </div>
       <div class="col-xs-12 col-sm-6 fl_icon">
         <div class="form-group fl_icon">
           <div class="icon"><i class="fa fa-envelope-o"></i></div>
-          <input class="form-input" type="text" placeholder="Your email" name="email">
+          <input class="form-input" type="text" placeholder="Your email" name="email" style="color: black;">
         </div>
       </div>
       
@@ -222,11 +222,11 @@ try {
     <div class="row">
       <div class="col-12">                 
         <div class="form-group">
-          <textarea class="form-input" required="" placeholder="Your text" name="text"></textarea>
+          <textarea class="form-input" required="" placeholder="Your text" name="text" style="color: black;"></textarea>
         </div>
       </div>
     </div>
-    <a class="btn btn-primary pull-right">submit</a>
+    <button class="btn btn-primary pull-right" type="submit">submit</button>
   </form>
 </div>
 </div>
